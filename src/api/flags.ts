bdc0f0ffc9ba157c48d4f112a8a32799acc4dd36
Chloe <Chloe.deng1000@gmail.com>
2023-01-24 23:35:50 -0500

    Suppressed errors from untranslated modules

--- src/api/flags.ts
+++ src/api/flags.ts
@@ -1,110 +1,134 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_1 = __importDefault(require("../user"));
-const flags_1 = __importDefault(require("../flags"));
+import user from '../user';
+import flags from '../flags';
+
 const flagsApi = module.exports;
+
+// The next line calls a function in a module that has not been updated to TS yet
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-flagsApi.create = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
+flagsApi.create = async (caller, data) => {
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     const required = ['type', 'id', 'reason'];
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     if (!required.every(prop => !!data[prop])) {
         throw new Error('[[error:invalid-data]]');
     }
+
     const { type, id, reason } = data;
-    yield flags_1.default.validate({
+
+    await flags.validate({
+        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         uid: caller.uid,
         type: type,
         id: id,
     });
+
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const flagObj = yield flags_1.default.create(type, id, caller.uid, reason);
+    const flagObj = await flags.create(type, id, caller.uid, reason);
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    flags_1.default.notify(flagObj, caller.uid);
+    flags.notify(flagObj, caller.uid);
+
     return flagObj;
-});
+};
+
+// The next line calls a function in a module that has not been updated to TS yet
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-flagsApi.update = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
+flagsApi.update = async (caller, data) => {
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const allowed = yield user_1.default.isPrivileged(caller.uid);
+    const allowed = await user.isPrivileged(caller.uid);
     if (!allowed) {
         throw new Error('[[error:no-privileges]]');
     }
+
     const { flagId } = data;
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     delete data.flagId;
+
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    yield flags_1.default.update(flagId, caller.uid, data);
-    return yield flags_1.default.getHistory(flagId);
-});
+    await flags.update(flagId, caller.uid, data);
+    return await flags.getHistory(flagId);
+};
+
+// The next line calls a function in a module that has not been updated to TS yet
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-flagsApi.appendNote = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
+flagsApi.appendNote = async (caller, data) => {
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const allowed = yield user_1.default.isPrivileged(caller.uid);
+    const allowed = await user.isPrivileged(caller.uid);
     if (!allowed) {
         throw new Error('[[error:no-privileges]]');
     }
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     if (data.datetime && data.flagId) {
         try {
+            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const note = yield flags_1.default.getNote(data.flagId, data.datetime);
+            const note = await flags.getNote(data.flagId, data.datetime);
+            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             if (note.uid !== caller.uid) {
                 throw new Error('[[error:no-privileges]]');
             }
-        }
-        catch (e) {
+        } catch (e) {
             // Okay if not does not exist in database
+            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             if (e.message !== '[[error:invalid-data]]') {
                 throw e;
             }
         }
     }
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    yield flags_1.default.appendNote(data.flagId, caller.uid, data.note, data.datetime);
-    const [notes, history] = yield Promise.all([
+    await flags.appendNote(data.flagId, caller.uid, data.note, data.datetime);
+    const [notes, history] = await Promise.all([
+        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        flags_1.default.getNotes(data.flagId),
+        flags.getNotes(data.flagId),
+        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        flags_1.default.getHistory(data.flagId),
+        flags.getHistory(data.flagId),
     ]);
     return { notes: notes, history: history };
-});
+};
+
+// The next line calls a function in a module that has not been updated to TS yet
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-flagsApi.deleteNote = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
+flagsApi.deleteNote = async (caller, data) => {
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const note = yield flags_1.default.getNote(data.flagId, data.datetime);
+    const note = await flags.getNote(data.flagId, data.datetime);
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     if (note.uid !== caller.uid) {
         throw new Error('[[error:no-privileges]]');
     }
+
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    yield flags_1.default.deleteNote(data.flagId, data.datetime);
+    await flags.deleteNote(data.flagId, data.datetime);
+    // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    yield flags_1.default.appendHistory(data.flagId, caller.uid, {
+    await flags.appendHistory(data.flagId, caller.uid, {
         notes: '[[flags:note-deleted]]',
         datetime: Date.now(),
     });
-    const [notes, history] = yield Promise.all([
+
+    const [notes, history] = await Promise.all([
+        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        flags_1.default.getNotes(data.flagId),
+        flags.getNotes(data.flagId),
+        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        flags_1.default.getHistory(data.flagId),
+        flags.getHistory(data.flagId),
     ]);
     return { notes: notes, history: history };
-});
+};
